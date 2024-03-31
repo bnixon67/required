4073c00aa9282c346685a9f4741033f00f511583
Bill Nixon <bnixon67@gmail.com>
2024-03-30 19:13:32 -0500

    cleanup

--- required_test.go
+++ required_test.go
@@ -2,15 +2,44 @@ package required_test
 
 import (
 	"errors"
+	"fmt"
 	"reflect"
 	"testing"
 
 	"github.com/bnixon67/required"
 )
 
+func ExampleCheck() {
+	type Address struct {
+		City  string
+		State string `required:"true"`
+		Zip   string
+	}
+
+	type Person struct {
+		Name  string `required:"true"`
+		Alias string `required:"false"`
+		Age   string
+		Address
+	}
+
+	person := Person{}
+
+	missingFields, err := required.Check(person)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		if len(missingFields) > 0 {
+			fmt.Println("Missing required fields:", missingFields)
+		} else {
+			fmt.Println("All required fields are set.")
+		}
+	}
+	// Output: Missing required fields: [Name Address.State]
+}
+
 func TestCheckRequiredDeepNested(t *testing.T) {
-	var nonStruct int
-	nonStruct = 1
+	var nonStruct = 1
 
 	type Street struct {
 		Number string `required:"true"`
